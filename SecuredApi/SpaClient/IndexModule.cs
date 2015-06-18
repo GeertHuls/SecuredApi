500bda635b6a79be18d2f915185ff74795be98e0
Geert Huls <geert.huls@macadam.eu>
2015-06-18 22:37:55 +0200

    Bare minimum for spa web client

---
+++ SecuredApi/SpaClient/IndexModule.cs
@@ -0,0 +1,15 @@
+﻿using Nancy;
+
+namespace SpaClient
+{
+    public class IndexModule : NancyModule
+    {
+        public IndexModule()
+        {
+            Get["/"] = parameters =>
+            {
+                return View["index"];
+            };
+        }
+    }
+}
---
+++ SecuredApi/SpaClient/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Owin;
+using Owin;
+using SpaClient;
+
+[assembly: OwinStartup(typeof(Startup))]
+
+namespace SpaClient
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseNancy();
+        }
+    }
+}
