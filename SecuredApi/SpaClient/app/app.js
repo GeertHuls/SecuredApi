bbdf1806235bb2f7b9bd19baa2437f8fea195fcf
Geert Huls <geert.huls@macadam.eu>
2015-06-24 22:49:40 +0200

    Initial angular page rendering but having issues with relative templateUrl path

---
+++ SecuredApi/SpaClient/app/app.js
@@ -0,0 +1,13 @@
+﻿(function () {
+    "use strict";
+
+    var app = angular.module("securedApi", ["ngRoute"]);
+
+    //use $location? http://stackoverflow.com/questions/13832529/how-to-config-routeprovider-and-locationprovider-in-angularjs
+    app.config(["$routeProvider", function ($routeProvider) {
+        $routeProvider.when("/", {
+            templateUrl: "/app/views/home.html"
+        });
+    }]);
+
+}());
