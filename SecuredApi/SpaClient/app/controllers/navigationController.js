e5c4ce8f0507cd302527fdc1f34427c90503956b
Geert Huls <geert.huls@macadam.eu>
2015-07-07 22:08:16 +0200

    Created navigation controller applying active css class when necessary

---
+++ SecuredApi/SpaClient/app/controllers/navigationController.js
@@ -0,0 +1,17 @@
+﻿(function(){
+    'use strict';
+
+    angular.module('securedApi')
+        .controller('navigationController',
+            ['$location',
+                navigationController]);
+
+    function navigationController ($location) {
+        var vm = this;
+
+        vm.isActive = function (route) {
+            var path = $location.path();
+            return path === route;
+        };
+    }
+})();
