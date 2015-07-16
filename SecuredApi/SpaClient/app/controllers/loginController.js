53c61e16692abeeb0694d870176f6b4c634fa11a
Geert Huls <geert.huls@macadam.eu>
2015-07-16 08:43:29 +0200

    Setup login controller

---
+++ SecuredApi/SpaClient/app/controllers/loginController.js
@@ -0,0 +1,16 @@
+﻿(function(){
+	'use strict';
+	
+	angular
+		.module('securedApi')
+		.controller('loginController',
+			[loginController]);
+
+	function loginController () {
+		var vm = this;
+
+		vm.test = function () {
+			console.log('test');
+		}
+	}
+})();
