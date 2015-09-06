6f8b2ec80499e7b9e72f3f778d76855c452cca54
Geert Huls <geert.huls@macadam.eu>
2015-09-06 22:46:29 +0200

    Configured facebook external provider as it would work using API2.3

--- SecuredApi/IdentityServer/Startup.cs
+++ SecuredApi/IdentityServer/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Security.Claims;
 using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
 using IdentityServer;
 using IdentityServer.Config;
 using IdentityServer.Helpers;
@@ -9,7 +10,9 @@ using IdentityServer3.Core.Configuration;
 using IdentityServer3.Core.Services;
 using IdentityServer3.Core.Services.Default;
 using Microsoft.Owin;
+using Microsoft.Owin.Security.Facebook;
 using Microsoft.Owin.Security.OpenIdConnect;
+using Newtonsoft.Json.Linq;
 using Owin;
 
 [assembly: OwinStartup(typeof(Startup))]
@@ -55,18 +58,61 @@ namespace IdentityServer
 
         private void ConfigureIdentityProviders(IAppBuilder app, string signInAsType)
         {
+            ConfigureAzureAdProvider(app, signInAsType);
+            ConfigureFacebookProvider(app, signInAsType);
+        }
+
+        private static void ConfigureFacebookProvider(IAppBuilder app, string signInAsType)
+        {
+            var options = new FacebookAuthenticationOptions
+            {
+                AuthenticationType = "Facebook",
+                Caption = "Facebook",
+                SignInAsAuthenticationType = signInAsType,
+                AppId = "XXXXXXXXXXXXXXXX",
+                AppSecret = "XXXXXXXXXXXXXXXX",
+                Provider = new FacebookAuthenticationProvider
+                {
+                    OnAuthenticated = context =>
+                    {
+                        JToken lastName, firstName;
+                        if (context.User.TryGetValue("last_name", out lastName))
+                        {
+                            context.Identity.AddClaim(new Claim(
+                                Constants.ClaimTypes.FamilyName,
+                                lastName.ToString()));
+                        }
+
+                        if (context.User.TryGetValue("first_name", out firstName))
+                        {
+                            context.Identity.AddClaim(new Claim(
+                                Constants.ClaimTypes.GivenName,
+                                firstName.ToString()));
+                        }
+
+                        context.Identity.AddClaim(new Claim("role", "Books"));
+                        context.Identity.AddClaim(new Claim("role", "Movies"));
+
+                        return Task.FromResult(0);
+                    }
+                }
+            };
+            options.Scope.Add("public_profile");
+            app.UseFacebookAuthentication(options);
+        }
+
+        private static void ConfigureAzureAdProvider(IAppBuilder app, string signInAsType)
+        {
             var aad = new OpenIdConnectAuthenticationOptions
             {
                 AuthenticationType = "aad",
                 Caption = "Azure AD",
                 SignInAsAuthenticationType = signInAsType,
-
                 Authority = "https://login.windows.net/xxxxxxxxxxxxxxx",
                 ClientId = "cdb67f7b-0a52-450b-a555-fe01ee12abc2",
                 RedirectUri = "https://secured.local:449/identityserver/core/aadcb",
                 PostLogoutRedirectUri = "https://secured.local:449/spaclient",
                 Scope = "openid profile email roles",
-                
                 Notifications = new OpenIdConnectAuthenticationNotifications
                 {
                     SecurityTokenValidated = async n =>
